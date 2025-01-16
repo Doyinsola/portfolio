7a68e42cc271c7dadfefc2154d014a4c06a263f7
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2025-01-16 18:30:32 -0500

    added routing for not found page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Skills from './pages/Skills/Skills';
 import Projects from './pages/Projects/Projects';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path='/skills' element={<Skills />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<About />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
