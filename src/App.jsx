0baa2a5bd3b9aafdb4617df905f1a9f24f638777
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2025-01-16 18:23:24 -0500

    added routing for the about page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Skills from './pages/Skills/Skills';
 import Projects from './pages/Projects/Projects';
+import About from './pages/About/About';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/projects' element={<Projects />} />
           <Route path='/skills' element={<Skills />} />
+          <Route path='/about' element={<About />} />
+
         </Routes>
       </BrowserRouter>
     </div>
